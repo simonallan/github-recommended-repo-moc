15a9f6b3689d298c470ad3405b41dd0641ccf64b
Simon Allan <simon.allan@cancer.org.uk>
2024-10-31 11:40:42 +0000

    initialise project

---
+++ test/github-repo-moc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GithubRepoMoc from '../lib/github-repo-moc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/github-repo-moc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GithubRepoMoc.GithubRepoMocStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
